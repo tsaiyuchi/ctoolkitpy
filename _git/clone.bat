a808c4c6477b158a3b01cff7869dc0ef69419e76
tsaiyuchi <tsaiyuchi88@gmail.com>
2025-10-06 10:56:46 +0000

    refactor

--- _git/clone.bat
+++ _git/clone.bat
@@ -7,3 +7,7 @@ cd github
 
 git clone -b develop2025 https://github.com/tsaiyuchi/ctoolkitpy.git
 cd ctoolkitpy
+
+git config user.name "tsaiyuchi"
+git config user.email "tsaiyuchi88@gmail.com"
+git config credential.helper store
