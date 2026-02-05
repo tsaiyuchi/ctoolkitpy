9a41dddce6cb53afb934bd6487fe162e09ee9779
tsaiyuchi <tsaiyuchi88@gmail.com>
2026-02-05 23:28:27 +0800

    refactor

--- _cmd/build_prepare.sh
+++
@@ -1,5 +0,0 @@
-
-
-apt install python3.10-venv -y
-pip3 install build
-
--- ctoolkitpy_v0_1/ctkftp.py
+++ ctoolkitpy_v0_1/ctkftp.py
@@ -1,5 +1,5 @@
 #--- basci packages
-
+import os
 
 #--- advanced packages ---
 import ftplib
@@ -27,29 +27,34 @@ class CtkFtpCfg:
         cfg_dict = json.loads(data)
         return cls(**cfg_dict)
 
+    def save_to_file(self, file_path: str):
+        me = self
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(me.to_json())
+    @classmethod
+    def load_from_file(cls, file_path: str):
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"找不到設定檔: {file_path}")
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = f.read()
+        return cls.from_json(data)
+
 
 
 class CtkFtp(ftplib.FTP):
 
-    
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self, host: str = "", user: str = "", passwd: str = "", port: int = 21, timeout: int = 30):
         me = self
-        super().__init__(*args, **kwargs)
-
-        me.config = CtkFtpCfg()
-        config = me.config
-        config.host = kwargs.get("host", args[0] if args else "")
-        config.port = kwargs.get("port", 21) #預設值21
-        config.user = kwargs.get("user", "")
-        config.passwd = kwargs.get("passwd", "")
+        # 初始化父類別，但不立即連線 (若要立即連線可將參數傳入 super)
+        super().__init__(timeout=timeout)
+        
+        me.config = CtkFtpCfg(
+            host=host,
+            port=port,
+            user=user,
+            passwd=passwd
+        )
     
-    def __del__(self):
-        try:
-            self.quit()
-        except Exception:
-            pass
-
     
     def connect_login(self, config:CtkFtpCfg=None):
         me = self
@@ -60,3 +65,14 @@ class CtkFtp(ftplib.FTP):
         me.connect(config.host, config.port)
         if config.user: me.login(config.user, config.passwd)
         else: me.login()  # 匿名登入
+
+
+#region Dispose --- --- ---
+    def dispose_close(self):
+        me = self
+        try: me.quit()
+        except: me.close()
+    def __del__(self): self.dispose_close()
+    def __enter__(self): return self
+    def __exit__(self, exc_type, exc_val, exc_tb): self.dispose_close()
+#endregion Dispose --- --- ---
