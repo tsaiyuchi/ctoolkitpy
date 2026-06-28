15e18897cc181df5ba1f0a814dca7ba3c006b9a5
yctsai85 <yctsai85@gmail.com>
2026-06-28 23:56:40 +0800

    uniform refactor

--- _git/clone.bat
+++ _git/clone.bat
@@ -1,8 +1,11 @@
 
 
+
 git clone -b develop2026 https://github.com/tsaiyuchi/ctoolkitpy.git
 cd ctoolkitpy
 git branch
-git config user.name "tsaiyuchi"
+git config user.name "tsaiyuchi88"
 git config user.email "tsaiyuchi88@gmail.com"
+git config pull.rebase false
 git config credential.helper store
+
